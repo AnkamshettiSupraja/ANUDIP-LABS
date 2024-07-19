0be01368b45d373466620f5cba36363ed44d0dbe
AnkamshettiSupraja <124861588+AnkamshettiSupraja@users.noreply.github.com>
2024-07-19 11:38:58 +0530

    Add files via upload

---
+++ TemperatureFtoC.java
@@ -0,0 +1,15 @@
+package firstproject;
+
+public class TemperatureFtoC {
+	public static void main(String[] args) {
+	        // Predefined value for temperature in Fahrenheit
+	        double fahrenheit = 98.6;
+
+	        // Convert Fahrenheit to Celsius
+	        double celsius = (fahrenheit - 32) * 5 / 9;
+
+	        // Output the result
+	        System.out.println("Temperature in Celsius: " + celsius + " °C");
+	    }
+	}
+	
